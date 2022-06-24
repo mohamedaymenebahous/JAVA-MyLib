222b085096408d8a94d2420c8a9dade5588b44cf
Aymene <aymanebahous04@gmail.com>
2022-06-24 05:19:01 +0100

    sec

--- src/test/java/dev/mohamedaymenebahous/util/Range.java
+++ src/test/java/dev/mohamedaymenebahous/util/Range.java
@@ -1,4 +1,34 @@
 package dev.mohamedaymenebahous.util;
 
 public class Range {
+
+    //produces sequence of n
+    public static int[] sequence(int n) {
+        int[] returnResult = new int[n];
+        for(int i = 0; i < n; i++) {
+            returnResult[i] = i;
+        }
+        return returnResult;
+    }
+
+    //produces sequence 'from' start to 'end'
+    public static int[] sequence(int start, int end) {
+        int size = end - start;
+        int[] returnResult = new int[size];
+        for(int i = 0; i < size; i++) {
+            returnResult[i] = start++;
+        }
+        return returnResult;
+    }
+
+    //produces sequence 'from' start to 'end' incrementing by a step
+    public static int[] sequence(int start, int end, int step) {
+        int size = end - start;
+        int[] returnResult = new int[size];
+        for(int i = 0; i < size; i++) {
+            returnResult[i] = start + (i * step);
+        }
+        return returnResult;
+    }
+
 }
